3073ccd4db31d50650f0dd4ca4c97708757d962c
shljsl75891 <shljsl75891@gmail.com>
2023-12-27 15:09:21 +0530

    add keybinding for switching git branches

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -10,5 +10,6 @@ return {
 		Remap("n", "<leader>fo", builtin.oldfiles, {})
 		Remap("n", "<leader>lg", builtin.live_grep, {})
 		Remap("n", "<leader>fr", builtin.registers, {})
+		Remap("n", "<leader>gb", builtin.git_branches, {})
 	end,
 }
