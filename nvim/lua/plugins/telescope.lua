4d4afdba2df339f4b8a3159c5f18959ffef93890
shljsl75891 <shljsl75891@gmail.com>
2024-01-24 21:01:54 +0530

    modify telescope keybindings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -9,7 +9,8 @@ return {
 		Remap("n", "<leader>fb", builtin.buffers, {})
 		Remap("n", "<leader>fo", builtin.oldfiles, {})
 		Remap("n", "<leader>lg", builtin.live_grep, {})
-		Remap("n", "<leader>fr", builtin.registers, {})
+		Remap("n", "<leader>cp", builtin.registers, {})
 		Remap("n", "<leader>gb", builtin.git_branches, {})
+		Remap("n", "<leader>ht", builtin.help_tags, {})
 	end,
 }
