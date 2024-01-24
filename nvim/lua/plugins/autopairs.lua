b641cd9653481355f443d2e9f8c0a892a0d59dd5
shljsl75891 <shljsl75891@gmail.com>
2024-01-24 21:06:34 +0530

    check treesitter for autopairs

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -2,9 +2,11 @@ return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
 	config = function()
-		require("nvim-autopairs").setup({})
-		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		require("nvim-autopairs").setup({
+			check_ts = true,
+		})
 		local cmp = require("cmp")
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 	end,
 }
