c78f94ce2f69483dec9255f3c62b2e7c72d63765
shljsl75891 <shljsl75891@gmail.com>
2024-02-02 20:07:19 +0530

    feat(awesome): cleaned up wibar

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -247,17 +247,16 @@ awful.screen.connect_for_each_screen(function(s)
 		layout = wibox.layout.align.horizontal,
 		{ -- Left widgets
 			layout = wibox.layout.fixed.horizontal,
-			mylauncher,
+			-- mylauncher,
 			s.mytaglist,
 			s.mypromptbox,
 		},
 		s.mytasklist, -- Middle widget
 		{ -- Right widgets
 			layout = wibox.layout.fixed.horizontal,
-			mykeyboardlayout,
 			wibox.widget.systray(),
 			mytextclock,
-			s.mylayoutbox,
+			-- s.mylayoutbox,
 		},
 	})
 end)
