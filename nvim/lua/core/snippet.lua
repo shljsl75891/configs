1cd3bb7acbfbc7da6c17c145d36c0ecee7f4b330
shljsl75891 <shljsl75891@gmail.com>
2024-02-02 17:50:00 +0530

    fix(snippet): disorting the h keypres

--- nvim/lua/core/snippet.lua
+++ nvim/lua/core/snippet.lua
@@ -1,5 +1,5 @@
 Remap(
 	"n",
-	"html",
+	"<leader>htb",
 	'i<!DOCTYPE html><CR><html lang="en"><CR><head><CR><meta charset="UTF-8"><CR><meta http-equiv="X-UA-Compatible" content="ie=edge"><CR><meta name="viewport" content="width=device-width, initial-scale=1.0"><CR><title></title><CR></head><CR><body></body><CR></html><ESC><CR>'
 )
