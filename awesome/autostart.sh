cef5f3da655237b435c8a1320f73864b3d2b6475
shljsl75891 <shljsl75891@gmail.com>
2024-02-08 10:02:50 +0530

    feat(awesome): random wallpapers

--- awesome/autostart.sh
+++ awesome/autostart.sh
@@ -9,6 +9,6 @@ function run {
 
 # programs
 run "picom"
-run "nitrogen --restore"
+run "nitrogen --set-scaled --random /home/sahil.jassal/Pictures/Wallpapers"
 run "nm-applet"
 run "blueman-applet"
