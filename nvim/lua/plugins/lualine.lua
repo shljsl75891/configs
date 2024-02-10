f356e9021394537d6491a2e562c3d3fe25150598
shljsl75891 <shljsl75891@gmail.com>
2024-02-10 13:36:18 +0530

    feat(nvim): lualine for seeing status

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,22 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				theme = "gruvbox-material",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff" },
+				lualine_c = { "diagnostics" },
+				lualine_x = { "filename" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
+}
