222b0e84fd2b389601493ab8e951f8c617e495e3
shljsl75891 <shljsl75891@gmail.com>
2024-02-10 17:14:12 +0530

    refactor(nvim): all handlers in lspconfig

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -6,66 +6,10 @@ return {
 	},
 	lazy = false,
 	config = function()
-		local lspconfig = require("lspconfig")
-		local lsp_capabilities = vim.tbl_deep_extend(
-			"force",
-			vim.lsp.protocol.make_client_capabilities(),
-			require("cmp_nvim_lsp").default_capabilities()
-		)
-		lsp_capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
-
-		local default_setup = function(server)
-			lspconfig[server].setup({
-				capabilities = lsp_capabilities,
-			})
-		end
-
-		local function organize_imports()
-			local params = {
-				command = "_typescript.organizeImports",
-				arguments = { vim.api.nvim_buf_get_name(0) },
-				title = "",
-			}
-			vim.lsp.buf.execute_command(params)
-		end
-
 		require("mason").setup({})
 		require("mason-lspconfig").setup({
 			ensure_installed = { "clangd", "tsserver", "lua_ls", "tailwindcss", "cssls", "emmet_ls" },
-			handlers = {
-				default_setup,
-				lua_ls = function()
-					lspconfig.lua_ls.setup({
-						capabilities = lsp_capabilities,
-						settings = {
-							Lua = {
-								runtime = {
-									version = "LuaJIT",
-								},
-								diagnostics = {
-									globals = { "vim", "awesome", "client" },
-								},
-								workspace = {
-									library = {
-										vim.env.VIMRUNTIME,
-									},
-								},
-							},
-						},
-					})
-				end,
-				tsserver = function()
-					lspconfig.tsserver.setup({
-						capabilities = lsp_capabilities,
-						commands = {
-							OrganizeImports = {
-								organize_imports,
-								description = "Organize Imports",
-							},
-						},
-					})
-				end,
-			},
+			automatic_installation = true,
 		})
 
 		require("mason-null-ls").setup({
