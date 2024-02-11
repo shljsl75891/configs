88d99fee03927ca42c051e425bdaec0ace0143ae
shljsl75891 <shljsl75891@gmail.com>
2024-02-11 20:23:36 +0530

    fix(nvim): annoying autopairs

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -4,9 +4,8 @@ return {
 	config = function()
 		require("nvim-autopairs").setup({
 			check_ts = true,
+			enable_check_bracket_line = false,
+			ignored_next_char = "[%w%.]",
 		})
-		local cmp = require("cmp")
-		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 	end,
 }
