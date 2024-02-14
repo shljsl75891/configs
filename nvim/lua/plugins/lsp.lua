af284008be9edad872b505e28919462b6ded4085
shljsl75891 <shljsl75891@gmail.com>
2024-02-14 16:58:14 +0530

    refactor(nvim): lsp and mason setup and handlers

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,14 +3,6 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		local builtin = require("telescope.builtin")
-		local lspconfig = require("lspconfig")
-
-		-- LSP completion capabilities
-		local capabilities = vim.tbl_deep_extend(
-			"force",
-			vim.lsp.protocol.make_client_capabilities(),
-			require("cmp_nvim_lsp").default_capabilities()
-		)
 
 		-- Diagnostics config
 		vim.diagnostic.config({
@@ -22,106 +14,32 @@ return {
 		})
 
 		-- Settings for LSP Attached buffer
-		local opts = { noremap = true, silent = true }
-		local on_attach = function(client, bufnr)
-			opts.buffer = bufnr
-			-- Lsp APIs
-			Remap("n", "K", vim.lsp.buf.hover, opts)
-			Remap("n", "<leader>fo", function()
-				require("conform").format({ async = true, lsp_fallback = true })
-			end)
-			Remap("n", "gd", builtin.lsp_definitions, opts)
-			Remap("n", "<leader>rr", builtin.lsp_references, opts)
-			Remap("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-			Remap("n", "<leader>rn", vim.lsp.buf.rename, opts)
-			Remap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-			-- Diagnostics APIs
-			Remap("n", "<leader>di", vim.diagnostic.open_float, opts)
-			Remap("n", "[d", vim.diagnostic.goto_prev, opts)
-			Remap("n", "]d", vim.diagnostic.goto_next, opts)
-		end
-
 		vim.api.nvim_create_autocmd("LspAttach", {
 			desc = "LSP actions",
-			callback = function(event) end,
-		})
-
-		local function organize_imports()
-			local params = {
-				command = "_typescript.organizeImports",
-				arguments = { vim.api.nvim_buf_get_name(0) },
-				title = "",
-			}
-			vim.lsp.buf.execute_command(params)
-		end
-
-		lspconfig["lua_ls"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = {
-				Lua = {
-					runtime = {
-						version = "LuaJIT",
-					},
-					diagnostics = {
-						globals = { "vim" },
-					},
-					workspace = {
-						library = {
-							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-							[vim.fn.stdpath("config") .. "/lua"] = true,
-						},
-					},
-				},
-			},
-		})
-
-		lspconfig["eslint"].setup({
-			settings = {
-				packageManager = "npm",
-			},
-			on_attach = function(client, bufnr)
-				vim.api.nvim_create_autocmd("BufWritePre", {
-					buffer = bufnr,
-					command = "EslintFixAll",
-				})
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function(ev)
+				local opts = { buffer = ev.buf, noremap = true, silent = true }
+				-- Lsp APIs
+				Remap("n", "K", vim.lsp.buf.hover, opts)
+				Remap("n", "<leader>fr", function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end, opts)
+				Remap("n", "gd", builtin.lsp_definitions, opts)
+				Remap("n", "<leader>rr", builtin.lsp_references, opts)
+				Remap("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+				Remap("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				Remap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+				-- Diagnostics APIs
+				Remap("n", "<leader>di", vim.diagnostic.open_float, opts)
+				Remap("n", "[d", vim.diagnostic.goto_prev, opts)
+				Remap("n", "]d", vim.diagnostic.goto_next, opts)
 			end,
 		})
 
-		lspconfig["tsserver"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			commands = {
-				OrganizeImports = {
-					organize_imports,
-					description = "Organize Imports",
-				},
-			},
-		})
-
-		lspconfig["tailwindcss"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			hovers = true,
-			suggestions = true,
-			root_dir = function(fname)
-				local root_pattern = require("lspconfig").util.root_pattern(
-					"tailwind.config.cjs",
-					"tailwind.config.js",
-					"postcss.config.js"
-				)
-				return root_pattern(fname)
-			end,
-		})
-
-		lspconfig["cssls"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-
-		lspconfig["emmet_ls"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
+		-- Remove trailing white spaces
+		vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+			pattern = "*",
+			command = [[%s/\s\+$//e]],
 		})
 	end,
 }
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -6,13 +6,80 @@ return {
 	},
 	lazy = false,
 	config = function()
+		local lspconfig = require("lspconfig")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+		local function organize_imports()
+			local params = {
+				command = "_typescript.organizeImports",
+				arguments = { vim.api.nvim_buf_get_name(0) },
+				title = "",
+			}
+			vim.lsp.buf.execute_command(params)
+		end
+
+		local default_setup = function(server)
+			require("lspconfig")[server].setup({
+				capabilities = capabilities,
+			})
+		end
+
 		require("mason").setup({})
 		require("mason-tool-installer").setup({
 			ensure_installed = { "prettierd", "stylua" },
 		})
 		require("mason-lspconfig").setup({
 			ensure_installed = { "eslint", "clangd", "tsserver", "lua_ls", "tailwindcss", "cssls", "emmet_ls" },
-			automatic_installation = true,
+			handlers = {
+				default_setup,
+				tsserver = function()
+					lspconfig["tsserver"].setup({
+						capabilities = capabilities,
+						commands = {
+							OrganizeImports = {
+								organize_imports,
+								description = "Organize Imports",
+							},
+						},
+					})
+				end,
+				tailwindcss = function()
+					lspconfig["tailwindcss"].setup({
+						capabilities = capabilities,
+						hovers = true,
+						suggestions = true,
+						root_dir = function(fname)
+							local root_pattern = require("lspconfig").util.root_pattern(
+								"tailwind.config.cjs",
+								"tailwind.config.js",
+								"postcss.config.js"
+							)
+							return root_pattern(fname)
+						end,
+					})
+				end,
+				lua_ls = function()
+					lspconfig["lua_ls"].setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								runtime = {
+									version = "LuaJIT",
+								},
+								diagnostics = {
+									globals = { "vim" },
+								},
+								workspace = {
+									library = {
+										[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+										[vim.fn.stdpath("config") .. "/lua"] = true,
+									},
+								},
+							},
+						},
+					})
+				end,
+			},
 		})
 	end,
 }
