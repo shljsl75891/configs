abca8ddc13104b575b9ae2467e9378523363ad74
shljsl75891 <shljsl75891@gmail.com>
2024-02-15 15:31:54 +0530

    feat(nvim): add split diff keybinding in fugitive

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,6 +1,9 @@
 return {
 	"tpope/vim-fugitive",
 	config = function()
+		Remap("n", "<leader>gd", function()
+			vim.cmd("Gdiffsplit")
+		end)
 		Remap("n", "<leader>gs", function()
 			vim.cmd("vertical G")
 		end)
