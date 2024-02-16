e5a7f1b898948274c2d29dc835156f5d632341a4
shljsl75891 <shljsl75891@gmail.com>
2024-02-16 11:45:46 +0530

    feat(nvim): update plugins and treesitter context

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,8 +2,18 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate",
+	dependencies = { "nvim-treesitter/nvim-treesitter-context" },
 	config = function()
 		require("nvim-treesitter.install").prefer_git = false
+
+		require("treesitter-context").setup({
+			enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+			mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+			line_numbers = true,
+			multiline_threshold = 1,
+		})
+
+		Remap("n", "<leader>tc", vim.cmd.TSContextToggle)
 
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
