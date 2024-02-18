5e0c3428400dd2ac313afad527115cd35b5b0e21
shljsl75891 <shljsl75891@gmail.com>
2024-02-18 10:43:45 +0530

    feat(awesome): rounded corners of all client windows

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -619,6 +619,9 @@ client.connect_signal("manage", function(c)
 	-- Set the windows at the slave,
 	-- i.e. put it at the end of others instead of setting it master.
 	-- if not awesome.startup then awful.client.setslave(c) end
+	c.shape = function(cr, w, h)
+		gears.shape.rounded_rect(cr, w, h, 8)
+	end
 
 	if awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
 		-- Prevent clients from being unreachable after screen count changes.
