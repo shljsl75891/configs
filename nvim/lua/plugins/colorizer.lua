0aaf7734d053f4ffad081b6c0abf488503bdb4a6
shljsl75891 <shljsl75891@gmail.com>
2024-02-23 09:40:05 +0530

    update(nvim): plugins, rm lualine, icons and add colorizer
    update many plugins
    was wrong about colorizer until configured by myself

---
+++ nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,17 @@
+return {
+	"NvChad/nvim-colorizer.lua",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("colorizer").setup({
+			user_default_options = {
+				names = false, -- "Name" codes like Blue or blue
+				css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+				css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+				-- Available methods are false / true / "normal" / "lsp" / "both"
+				-- True is same as normal
+				tailwind = true, -- Enable tailwind colors
+				sass = { enable = true, parsers = { "css" } }, -- Enable sass colors
+			},
+		})
+	end,
+}
