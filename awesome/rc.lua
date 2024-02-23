38e982230ab475d7eb4e6a0e0b4d1f0755d8207f
shljsl75891 <shljsl75891@gmail.com>
2024-02-23 21:37:34 +0530

    core(awesome): try new layout

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -79,14 +79,14 @@ local modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
 	-- awful.layout.suit.floating,
-	awful.layout.suit.tile,
+	-- awful.layout.suit.tile,
 	-- awful.layout.suit.tile.left,
 	-- awful.layout.suit.tile.bottom,
 	-- awful.layout.suit.tile.top,
 	-- awful.layout.suit.fair,
 	-- awful.layout.suit.fair.horizontal,
 	-- awful.layout.suit.spiral,
-	-- awful.layout.suit.spiral.dwindle,
+	awful.layout.suit.spiral.dwindle,
 	-- awful.layout.suit.max,
 	-- awful.layout.suit.max.fullscreen,
 	-- awful.layout.suit.magnifier,
@@ -660,6 +660,9 @@ awful.rules.rules = {
 -- {{{ Signals
 -- Signal function to execute when a new client appears.
 client.connect_signal("manage", function(c)
+	if not awesome.startup then
+		awful.client.setslave(c)
+	end
 	-- Set the windows at the slave,
 	-- i.e. put it at the end of others instead of setting it master.
 	-- if not awesome.startup then awful.client.setslave(c) end
