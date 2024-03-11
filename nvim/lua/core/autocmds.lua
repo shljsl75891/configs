57be0b19c148e799dcaa0ffea76a29cb2babdacc
shljsl75891 <shljsl75891@gmail.com>
2024-03-11 22:07:48 +0530

    feat(nvim): augroup, heard it is good to have

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -1,6 +1,8 @@
+local group = vim.api.nvim_create_augroup("MarkDownNotes", { clear = true })
 -- autocmd for easy navigation markdown files
 vim.api.nvim_create_autocmd("BufEnter", {
 	pattern = "*.md",
+	group = group,
 	callback = function(ev)
 		vim.opt_local.wrap = true
 		vim.api.nvim_buf_set_keymap(ev.buf, "n", "j", "gj", {})
