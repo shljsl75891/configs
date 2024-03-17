14e1b8faebb8edf13cbcc72a064687259cfc43bc
shljsl75891 <shljsl75891@gmail.com>
2024-03-17 11:14:12 +0530

    attempt(nvim): to fix slow lsp performance

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -9,6 +9,7 @@ return {
 		local lspconfig = require("lspconfig")
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+		capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
 
 		local function organize_imports()
 			local params = {
