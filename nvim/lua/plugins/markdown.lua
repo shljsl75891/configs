02144e68ff83b553dd7db9aeee020e28e164fa50
shljsl75891 <shljsl75891@gmail.com>
2024-03-17 18:25:20 +0530

    feat(nvim): image paste plugin for notes in .md

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,8 +1,20 @@
 return {
 	"iamcco/markdown-preview.nvim",
+	dependencies = {
+		"dfendr/clipboard-image.nvim",
+	},
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 	ft = { "markdown" },
 	build = function()
 		vim.fn["mkdp#util#install"]()
+	end,
+	config = function()
+		require("clipboard-image").setup({
+			markdown = {
+				img_dir_txt = "../img",
+			},
+
+			Remap("n", "<leader>p", vim.cmd.PasteImg),
+		})
 	end,
 }
