28074d8ae5b4f8a17965b325c931749c34968f7e
shljsl75891 <shljsl75891@gmail.com>
2024-03-17 11:16:51 +0530

    refactor(nvim): more readable

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -33,53 +33,18 @@ return {
 				LspReferenceRead = { link = "Visual" },
 				LspReferenceText = { link = "Visual" },
 				LspReferenceWrite = { link = "Visual" },
-				LspSignatureActiveParameter = {
-					bg = nil,
-					fg = palette.bright_yellow,
-					bold = true,
-				},
-				NormalFloat = {
-					bg = palette.dark0_soft,
-				},
-				TelescopeNormal = {
-					bg = palette.dark0,
-					fg = palette.dark0,
-				},
-				TelescopeResultsNormal = {
-					bg = palette.dark0,
-					fg = palette.light4,
-				},
-				TelescopeBorder = {
-					bg = palette.dark0,
-					fg = palette.light0_soft,
-				},
-				TelescopeSelection = {
-					bg = palette.dark0_soft,
-					fg = palette.light0_soft,
-				},
-				TelescopePromptNormal = {
-					bg = palette.dark0_hard,
-				},
-				TelescopePromptBorder = {
-					bg = palette.dark0_hard,
-					fg = palette.dark0_hard,
-				},
-				TelescopePromptTitle = {
-					bg = palette.dark0_hard,
-					fg = palette.dark0_hard,
-				},
-				TelescopePreviewTitle = {
-					bg = palette.dark0,
-					fg = palette.dark0,
-				},
-				TelescopeResultsTitle = {
-					bg = palette.dark0,
-					fg = palette.dark0,
-				},
-				TelescopeMatching = {
-					fg = palette.bright_aqua,
-					bold = true,
-				},
+				LspSignatureActiveParameter = { bg = nil, fg = palette.bright_yellow, bold = true },
+				NormalFloat = { bg = palette.dark0_soft },
+				TelescopeNormal = { bg = palette.dark0, fg = palette.dark0 },
+				TelescopeResultsNormal = { bg = palette.dark0, fg = palette.light4 },
+				TelescopeBorder = { bg = palette.dark0, fg = palette.light0_soft },
+				TelescopeSelection = { bg = palette.dark0_soft, fg = palette.light0_soft },
+				TelescopePromptNormal = { bg = palette.dark0_hard },
+				TelescopePromptBorder = { bg = palette.dark0_hard, fg = palette.dark0_hard },
+				TelescopePromptTitle = { bg = palette.dark0_hard, fg = palette.dark0_hard },
+				TelescopePreviewTitle = { bg = palette.dark0, fg = palette.dark0 },
+				TelescopeResultsTitle = { bg = palette.dark0, fg = palette.dark0 },
+				TelescopeMatching = { fg = palette.bright_aqua, bold = true },
 			},
 			transparent_mode = true,
 		})
