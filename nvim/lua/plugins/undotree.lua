f1b7368f35944a991e545b7e2e35ca42cb68116e
shljsl75891 <shljsl75891@gmail.com>
2024-04-03 08:10:42 +0530

    feat: undotree and update plugins

---
+++ nvim/lua/plugins/undotree.lua
@@ -0,0 +1,15 @@
+return {
+	"mbbill/undotree",
+	config = function()
+		local undo_tree_opts = {
+			undotree_SetFocusWhenToggle = 1,
+		}
+
+		for k, v in pairs(undo_tree_opts) do
+			vim.g[k] = v
+		end
+
+		local opts = { noremap = true, silent = true }
+		Remap("n", "<leader>u", vim.cmd.UndotreeToggle, opts)
+	end,
+}
