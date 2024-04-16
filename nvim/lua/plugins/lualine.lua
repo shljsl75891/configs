0eea4ed2239912ca1a88f397c4a00437cb3a75eb
shljsl75891 <shljsl75891@gmail.com>
2024-04-16 21:31:05 +0530

    update: some configs

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -10,7 +10,7 @@ return {
 			lualine_a = { "mode" },
 			lualine_b = { "branch", "diff", "diagnostics" },
 			lualine_c = { { "filename", path = 1 } },
-			lualine_x = { "encoding", "filetype" },
+			lualine_x = { "filetype" },
 			lualine_y = { "progress" },
 			lualine_z = { "location" },
 		},
