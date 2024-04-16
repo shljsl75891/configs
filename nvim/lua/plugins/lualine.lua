b7494f12223eec0f56b23f1467dc264de86c331c
shljsl75891 <shljsl75891@gmail.com>
2024-04-16 18:09:14 +0530

    fix(nvim): relative path for current file lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -6,5 +6,13 @@ return {
 			icons_enabled = true,
 			theme = "gruvbox-material",
 		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_c = { { "filename", path = 1 } },
+			lualine_x = { "encoding", "filetype" },
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
+		},
 	},
 }
