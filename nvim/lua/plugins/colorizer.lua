eab86c5249ccd13050a288d05d0c6fc0d3f09b0a
shljsl75891 <shljsl75891@gmail.com>
2025-02-08 13:59:17 +0530

    feat(nvim): maintained colorizer plugin

---
+++ nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,15 @@
+return {
+	"catgoose/nvim-colorizer.lua",
+	event = "BufReadPre",
+	opts = {
+		lazy_load = true,
+		user_default_options = {
+			names = false,
+			RGB = true,
+			RGBA = false,
+			RRGGBB = true,
+			RRGGBBAA = false,
+			AARRGGBB = false,
+		},
+	},
+}
