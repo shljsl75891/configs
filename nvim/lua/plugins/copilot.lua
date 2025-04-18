1bc6ce5c7e5ffd0bbfda1bd5fdde6fe1bad7638c
shljsl75891 <shljsl75891@gmail.com>
2025-04-18 07:01:46 +0530

    feat(nvim): copilot plugin

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,20 @@
+return {
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	opts = {
+		suggestion = {
+			enabled = true,
+			auto_trigger = false,
+			debounce = 75,
+			trigger_on_accept = true,
+			keymap = {
+				accept = "<C-y>",
+				next = "<M-]>",
+				prev = "<M-[>",
+				dismiss = "<C-]>",
+			},
+		},
+		panel = { enabled = false },
+	},
+}
