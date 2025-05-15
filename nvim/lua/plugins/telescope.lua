20a89d3bd9b50a4ac5c2e6be4e05d3223cb3ac5f
shljsl75891 <shljsl75891@gmail.com>
2025-05-15 17:58:59 +0530

    fix(nvim): update build command for telescope fzf

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -60,7 +60,10 @@ return {
 	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+		},
 	},
 	config = function()
 		require("configs.telescope")
