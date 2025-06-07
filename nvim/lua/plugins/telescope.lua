52566ef08ac647f734f53a9fe46cabfe803827ba
shljsl75891 <shljsl75891@gmail.com>
2025-06-07 11:48:07 +0530

    fix(nvim): remove oil plugin

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -60,7 +60,6 @@ return {
 	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
 		{
 			"nvim-telescope/telescope-fzf-native.nvim",
 			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
