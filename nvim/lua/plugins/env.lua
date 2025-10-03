527ac6956a8c3b79a65d056c1656637d118970ee
shljsl75891 <shljl75891@gmail.com>
2025-10-03 15:16:21 +0530

    feat(nvim): cloak for env secrets

--- nvim/lua/plugins/env.lua
+++ nvim/lua/plugins/env.lua
@@ -1,6 +1,25 @@
 return {
 	"tpope/vim-dotenv",
 	lazy = false,
+	keys = {
+		{
+			"<leader>te",
+			function()
+				vim.cmd("CloakToggle")
+			end,
+			desc = "[T]oggle [E]nv Cloak",
+		},
+	},
+	dependencies = {
+		{
+			"laytan/cloak.nvim",
+			opts = {
+				enabled = true,
+				cloak_character = "*",
+				cloak_telescope = true,
+			},
+		},
+	},
 	config = function()
 		vim.cmd("Dotenv " .. vim.fn.stdpath("config") .. "/.env")
 	end,
