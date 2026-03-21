6f3bb2d4f0213422e556f635b5ed020b6963a103
shljsl75891 <shljsl75891@gmail.com>
2026-03-21 17:57:12 +0530

    fix(nvim): lazy loading of render markdown plugin

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -26,6 +26,7 @@ return {
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" },
+		ft = { "markdown" },
 		opts = {},
 	},
 }
