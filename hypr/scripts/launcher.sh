92f7ae5ef195df9c1064deb8d493665e41423045
shljsl75891 <shljsl75891@gmail.com>
2026-06-20 23:49:45 +0530

    fix(hypr): adjust the looks of bemenu

--- hypr/scripts/launcher.sh
+++ hypr/scripts/launcher.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 bemenu-run \
   -i \
-  -p "  " \
+  -p "Execute >" \
   --fn "NotoSans Nerd Font 10" \
-  -H 32 \
+  -H 24 \
+  -M 8 \
   -W 1 \
-  --hp 8 \
+  --hp 6 \
+  --cw 8 \
+  --ch 16 \
   --tb "#1d2021" --tf "#a89984" \
   --fb "#1d2021" --ff "#d4be98" \
   --nb "#1d2021" --nf "#d4be98" \
