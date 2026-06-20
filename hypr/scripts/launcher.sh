3feeefc2045e2b8a2479fe898690c78254181373
shljsl75891 <shljsl75891@gmail.com>
2026-06-20 15:37:31 +0530

    refactor(launcher): replace rofi/wofi with bemenu, align gruvbox material palette
    - Remove rofi and wofi configs entirely
    - Add bemenu-run launcher script with gruvbox material dark hard colors
    - Add Super+Shift+L keybinding for hyprlock
    - Remove powermenu module from waybar, fix tray right padding
    - Align hyprlock colors to gruvbox material palette

---
+++ hypr/scripts/launcher.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+bemenu-run \
+  -i \
+  -p "  " \
+  --fn "NotoSans Nerd Font 10" \
+  -H 32 \
+  -W 1 \
+  --hp 8 \
+  --tb "#1d2021" --tf "#a89984" \
+  --fb "#1d2021" --ff "#d4be98" \
+  --nb "#1d2021" --nf "#d4be98" \
+  --hb "#a9b665" --hf "#1d2021" \
+  --sb "#a9b665" --sf "#1d2021" \
+  --ab "#1d2021" --af "#d4be98" \
+  --fbb "#1d2021" --fbf "#d4be98" \
+  --bdr "#504945" -B 1
